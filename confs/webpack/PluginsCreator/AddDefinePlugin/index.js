449acfa1a46087dee850f361258c4e3c44f524a9
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-02-09 11:23:46 +0800

    fix: some mistakes

--- confs/webpack/PluginsCreator/AddDefinePlugin/index.js
+++ confs/webpack/PluginsCreator/AddDefinePlugin/index.js
@@ -5,8 +5,12 @@ const defaultConf = Object.freeze({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 });
 
-// use eslint-webpack-plugin
-const createDefinePlugin = (conf = defaultConf) => new DefinePlugin(conf);
+// use define-plugin
+const createDefinePlugin = (conf = {}) =>
+    new DefinePlugin({
+        ...defaultConf,
+        ...conf,
+    });
 
 module.exports = {
     createDefinePlugin,
