4362f0d6c04c0c759625ef5c892e57fb2372f28d
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-02-15 15:07:19 +0800

    fix: condition fix and ts checker doc

--- confs/CheckNodejsVersion.ts
+++ confs/CheckNodejsVersion.ts
@@ -2,15 +2,16 @@ import { versions, release } from 'process';
 
 /** @description Check node.js version before running programme. */
 export const checkNodejsVersion = () => {
-    const { node: nodeV } = versions;
-    const [greatestVersion] = nodeV.split('.').map(v => +v);
+    const [greatestVersion] = versions.node.split('.').map(v => +v);
 
+    // Check the greatest version is equal to or more than 14.
     if (greatestVersion < 14) {
         throw new Error('The Nodejs version should be >= 14');
     }
 
+    // Check whether is LTS version or not.
     const { lts } = release;
-    if (greatestVersion % 2 === 1 && !lts) {
+    if (greatestVersion % 2 === 1 || !lts) {
         throw new Error('Must use LTS version!');
     }
 
